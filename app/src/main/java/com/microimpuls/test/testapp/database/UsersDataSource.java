bb7ab438fb4437b93f60e4e1ebeab5981a3eb04e
anton <byghoster@gmail.com>
2018-12-14 02:33:54 +0500

    I can't return a single value

--- app/src/main/java/com/microimpuls/test/testapp/database/UsersDataSource.java
+++ app/src/main/java/com/microimpuls/test/testapp/database/UsersDataSource.java
@@ -1,12 +1,14 @@
 package com.microimpuls.test.testapp.database;
 
 import android.database.Cursor;
+
 import com.microimpuls.test.testapp.BasicUserInfo;
+
 import java.util.List;
 
 interface UsersDataSource<T extends BasicUserInfo> {
 
-	long addUsers( List<T> users );
+    void addUsers(List<T> users);
 
 	long addUser( long userId, String username, int age );
 
